c740d0d2216bc14486efc50db3ce8d5aa1b33d5a
MORI Shingo <cos@kmc.gr.jp>
2011-10-05 02:51:58 +0900

    AhoCorasick becomes class

--- AhoCorasick.cpp
+++ AhoCorasick.cpp
@@ -1,86 +1,95 @@
-struct PMA {
-  PMA *next[128]; // next[0] is for fail
-  vector<int> accept;
-  PMA() { fill(next, next + 128, (PMA*)NULL); }
+struct AhoCorasick {
+  static const int SIZE = 128;
+  struct State {
+    int index;
+    int next[SIZE]; // next[0] is failuer link
+    vector<int> accept;
+    State(int index) : index(index) {
+      memset(next, -1, sizeof(next));
+    }
+  };
+
+  bool build;
+  vector<State> pma;
+  vector<int> lens;
+  //vector<string. strs;
+
+  AhoCorasick() {
+    build = false;
+    pma.clear();
+    pma.push_back(State(0));
+    lens.clear();
+    //strs.clear();
+  }
+
+  void AddString(const char *str) {
+    assert(!build);
+    int t = 0;
+    for (int i = 0; str[i]; i++) {
+      int c = str[i];
+      if (pma[t].next[c] == -1) {
+        int m = pma.size();
+        pma[t].next[c] = m;
+        pma.push_back(State(m));
+      }
+      t = pma[t].next[c];
+    }
+    pma[t].accept.push_back(lens.size());
+    lens.push_back(strlen(str));
+    //strs.push_back(str);
+  }
+
+  void BuildPMA() {
+    assert(!build);
+    queue<int> que;  // make failure link using bfs
+    for (int c = 1; c < SIZE; c++) {
+      if (pma[0].next[c] != -1) {
+        pma[pma[0].next[c]].next[0] = 0;
+        que.push(pma[0].next[c]);
+      } else {
+        pma[0].next[c] = 0;
+      }
+    }
+    while (!que.empty()) {
+      int t = que.front();
+      que.pop();
+      for (int c = 1; c < SIZE; c++) {
+        if (pma[t].next[c] != -1) {
+          que.push(pma[t].next[c]);
+          int r = pma[t].next[0];
+          while (pma[r].next[c] == -1) {
+            r = pma[r].next[0];
+          }
+          pma[pma[t].next[c]].next[0] = pma[r].next[c];
+          for (vector<int>::iterator it = pma[pma[r].next[c]].accept.begin(); it != pma[pma[r].next[c]].accept.end(); it++) {
+            pma[pma[t].next[c]].accept.push_back(*it);
+          }
+        }
+      }
+    }
+    build = true;
+  }
+
+  int OneMove(int index, int c) {
+    return pma[index].next[c] != -1 ?
+      pma[index].next[c] :
+      pma[index].next[c] = OneMove(pma[index].next[0], c);
+    //while (pma[index].next[c] == -1) { index = pma[index].next[0]; }
+    //return pma[index].next[c];
+  }
+
+  vector<int> Match(const char *t) {
+    int index = 0;
+    vector<int> ret(lens.size());
+    int n = strlen(t);
+    for (int i = 0; i < n; i++) {
+      int c = *t;
+      OneMove(index, c);
+      for (vector<int>::const_iterator it = pma[index].accept.begin(); it != pma[index].accept.end(); it++) {
+        if (ret[*it] != -1) { continue; }
+        ret[*it] = i - lens[*it] + 1;
+      }
+    }
+    return ret;
+  }
 };
-
-PMA *buildPMA(char p[1000][1000], int size) {
-  PMA *root = new PMA;
-  for (int i = 0; i < size; i++) { // make trie
-    PMA *t = root;
-    for (int j = 0; p[i][j]; j++) {
-      int c = p[i][j];
-      if (t->next[c] == NULL) {
-        t->next[c] = new PMA;
-      }
-      t = t->next[c];
-    }
-    t->accept.push_back(i);
-  }
-  queue<PMA*> que; // make failure link using bfs
-  for (int c = 1; c < 128; c++) {
-    if (root->next[c]) {
-      root->next[c]->next[0] = root;
-      que.push(root->next[c]);
-    } else {
-      root->next[c] = root;
-    }
-  }
-  while (!que.empty()) {
-    PMA *t = que.front();
-    que.pop();
-    for (int c = 1; c < 128; c++) {
-      if (t->next[c] != NULL) {
-        que.push(t->next[c]);
-        PMA *r = t->next[0];
-        while (!r->next[c]) {
-          r = r->next[0];
-        }
-        t->next[c]->next[0] = r->next[c];
-        for (vector<int>::iterator it = r->next[c]->accept.begin(); it != r->next[c]->accept.end(); it++) {
-          t->next[c]->accept.push_back(*it);
-        }
-      }
-    }
-  }
-  return root;
-}
-
-void deletePMA(PMA *root) {
-  set<PMA*> visit;
-  queue<PMA*> que;
-  que.push(root);
-  visit.insert(root);
-  while (!que.empty()) {
-    PMA *t = que.front();
-    que.pop();
-    for (int c = 1; c < 128; c++) {
-      if (t->next[c] == NULL) { continue; }
-      if (visit.count(t->next[c])) { continue; }
-      visit.insert(t->next[c]);
-      que.push(t->next[c]);
-    }
-  }
-  for (set<PMA*>::iterator it = visit.begin(); it != visit.end(); it++) {
-    delete *it;
-  }
-}
-
-// n = strlen(t)
-// m[i] = strlen(p[i])
-vector<int> match(const char *t, int n, const PMA *v, int m[], int size) {
-  vector<int> ret(size, -1);
-  //int n = strlen(t);
-  for (int i = 0; i < n; i++) {
-    int c = t[i];
-    while (!v->next[c]) {
-      v = v->next[0];
-    }
-    v = v->next[c];
-    for (vector<int>::const_iterator it = v->accept.begin(); it != v->accept.end(); it++) {
-      if (ret[*it] != -1) { continue; }
-      ret[*it] = i - m[*it] + 1;
-    }
-  }
-  return ret;
-}
