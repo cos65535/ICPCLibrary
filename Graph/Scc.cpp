f83846c70555e8290147f82b3da03ed2c10c3282
MORI Shingo <cos@kmc.gr.jp>
2011-10-11 00:34:25 +0900

    Soc -> Scc

--- Graph/Scc.cpp
+++ Graph/Scc.cpp
@@ -1,13 +1,13 @@
-void SocDfs(const Graph &g, int from, vector<int> &visit, vector<int> &st) {
+void SccDfs(const Graph &g, int from, vector<int> &visit, vector<int> &st) {
   visit[from] = 1;
   for (Edges::const_iterator it = g[from].begin(); it != g[from].end(); it++) {
     if (visit[it->dest]) { continue; }
-    SocDfs(g, it->dest, visit, st);
+    SccDfs(g, it->dest, visit, st);
   }
   st.push_back(from);
 }
 
-vector<vector<int> > Soc(const Graph &g) {
+vector<vector<int> > Scc(const Graph &g) {
   const int n = g.size();
   vector<vector<int> > ret;
   Graph revg(n);
@@ -20,36 +20,36 @@ vector<vector<int> > Soc(const Graph &g) {
   vector<int> visit(n, 0);
   for (int i = 0; i < n; i++) {
     if (visit[i]) { continue; }
-    SocDfs(g, i, visit, st);
+    SccDfs(g, i, visit, st);
   }
   visit = vector<int>(n, 0);
   for (int i = n - 1; i >= 0; i--) {
     int index = st[i];
     if (visit[index]) { continue; }
     vector<int> nret;
-    SocDfs(revg, index, visit, nret);
+    SccDfs(revg, index, visit, nret);
     ret.push_back(nret);
   }
   return ret;
 }
 
 // select min weight edge
-Graph SocGraph(const Graph &g) {
-  vector<vector<int> > soc = Soc(g);
+Graph SccGraph(const Graph &g) {
+  vector<vector<int> > scc = Scc(g);
   const int n = g.size();
-  const int m = soc.size();
+  const int m = scc.size();
   vector<int> mapto(n);
-  for (int i = 0; i < (int)soc.size(); i++) {
-    for (int j = 0; j < (int)soc[i].size(); j++) {
-      mapto[soc[i][j]] = i;
+  for (int i = 0; i < (int)scc.size(); i++) {
+    for (int j = 0; j < (int)scc[i].size(); j++) {
+      mapto[scc[i][j]] = i;
     }
   }
   Graph ret(m);
   vector<int> indexs(m, -1);
   for (int from = 0; from < m; from++) {
     int e = 0;
-    for (int i = 0; i < (int)soc[from].size(); i++) {
-      for (Edges::const_iterator it = g[soc[from][i]].begin(); it != g[soc[from][i]].end(); it++) {
+    for (int i = 0; i < (int)scc[from].size(); i++) {
+      for (Edges::const_iterator it = g[scc[from][i]].begin(); it != g[scc[from][i]].end(); it++) {
         int to = mapto[it->dest];
         if (from == to) { continue; } // loopback
         if (indexs[to] == -1) {
