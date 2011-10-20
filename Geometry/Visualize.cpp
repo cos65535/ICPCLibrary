219b9cb1e519c5e25dc38e1303d0e0198e0c8f11
MORI Shingo <cos@kmc.gr.jp>
2011-10-20 21:00:51 +0900

    modified Geometry/Visualize.cpp

--- Geometry/Visualize.cpp
+++ Geometry/Visualize.cpp
@@ -8,9 +8,8 @@ void ChangeColor(int r, int g, int b) {
   fprintf(stderr, "c.strokeStyle = 'rgb(%d, %d, %d)';\n", r, g, b);
 }
 void DrawPoint(const Point &p) {
-  fprintf(stderr, "fillRect(%d, %d, %d, %d)\n",
-      (int)(zoom*(p.real()-2)), 1980-(int)(zoom*(p.imag() - 2)),
-      (int)(zoom*(p.real()+2)), 1980-(int)(zoom*(p.imag() + 2)));
+  fprintf(stderr, "circle(%d, %d, %d)\n",
+      (int)(zoom*p.real()), 1980-(int)(zoom*p.imag()), 2);
 }
 void DrawLine(const Line &l) {
   fprintf(stderr, "line(%d, %d, %d, %d)\n",
