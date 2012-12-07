16c2672cdbfbcfc9abd5ccab9aac09066825645c
MORI Shingo <cos@kmc.gr.jp>
2012-12-07 16:08:34 +0900

    update XorShift

--- XorShift.cpp
+++ XorShift.cpp
@@ -1,15 +1,19 @@
-unsigned int Xor128(void) { 
-  static unsigned int x = 0x34fb2383;
-  static unsigned int y = 0x327328fa;
-  static unsigned int z = 0xabd4b54a;
-  static unsigned int w = 0xa9dba8d1; 
-  unsigned int t;
-       
-  t = x ^ (x << 11);
-  x = y; y = z; z = w;
-  return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); 
-}
 struct Random {
+  unsigned int x;
+  unsigned int y;
+  unsigned int z;
+  unsigned int w; 
+  Random() : x(0x34fb2383), y(0x327328fa), z(0xabd4b54a), w(0xa9dba8d1) {;}
+  void Seed(int s) {
+    *this = Random();
+    w = s;
+  }
+  unsigned int Xor128() {
+    unsigned int t;
+    t = x ^ (x << 11);
+    x = y; y = z; z = w;
+    return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)); 
+  }
   int next(int r) { return Xor128() % r; }
   int next(int l, int r) { return next(r - l + 1) + l; }
   ll next(ll r) { return (ll)((((unsigned long long)Xor128() << 32) + (unsigned long long)Xor128()) % r); }
