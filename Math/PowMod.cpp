7758e95c7df93b73b842eb4b8011a69772d04102
MORI Shingo <cos@kmc.gr.jp>
2016-09-17 18:25:12 +0900

    ll -> int

--- Math/PowMod.cpp
+++ Math/PowMod.cpp
@@ -1,4 +1,4 @@
-long long powmod(long long base, long long power, long long mod) {
+long long powmod(long long base, long long power, int mod) {
   long long ans = 1;
   while (power > 0) {
     if (power & 1) { ans = ans * base % mod; }
